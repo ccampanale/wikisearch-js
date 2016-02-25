8036153292a84abcdd677042605fc6b5facd7694
Christopher Campanale <ccampanale@gmail.com>
2016-02-25 16:01:26 -0500

    added configuration support

--- server/app.js
+++ server/app.js
@@ -2,18 +2,27 @@
 // WikiSearch entry
 
 var pjson      = require('../package.json');
+var config     = require('./config');
 var express    = require('express');
 var app        = express();
 var bodyParser = require('body-parser');
 var path       = require('path');
-var clim       = require("clim");
+var clim       = require('clim');
 
 var logger    = clim("[WikiSearch-"+pjson.version+"]");
+var console   = clim("(SERVER):", logger);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 3000;
+// pass config to express app
+app.config = config;
+
+// pass config to routes
+app.use(function(req,res,next){
+  req.config = config;
+  next();
+});
 
 var api_router = require('./routes/repos');
 var ui_router = require('./routes/ui');
@@ -22,8 +31,8 @@ app.use('/v1', api_router);
 app.use('/',   ui_router);
 app.use('/ui', express.static(path.resolve('server/static')));
 
-app.listen(port);
-logger.info('WikiSearch istening on: ' + port);
+app.listen(app.config.server.port);
+console.info('WikiSearch istening on: ' + app.config.server.port);
 
 // export server for testing/consumption
 module.exports = app;
---
+++ server/config.js
@@ -0,0 +1,74 @@
+// config.js
+// Configuration component that sets defaults or uses any configuration
+// provided in configuration file or environment variables.
+
+"use strict";
+
+var pjson       = require('../package.json');
+var fs          = require("fs");
+var file_config = {};
+var clim        = require('clim');
+var logger      = clim("[WikiSearch-"+pjson.version+"]");
+
+var console = clim("(CONF):", logger);
+console.info('Loading configuration');
+
+// Conditionally import a config file that may be at the root of the project
+var config_exists = fs.existsSync('config.json');
+console.log(config_exists);
+if(config_exists){
+
+    // found a config file
+    console.info('Found local configuration file...');
+
+    try{
+        // read in the config file
+        file_config = JSON.parse(fs.readFileSync('config.json'));
+    }catch (e){
+        console.error('Error found in configuration file: ' + e);
+    }
+
+}else{
+
+    // found a config file
+    console.info('No local configuration file found...');
+
+}
+
+// base config object
+var config = {};
+
+// All config options will be pulled in the order:
+// file_config -> process environment -> default setting
+
+// server settings
+config.server = {};
+config.server.port = process.env.PORT || 3000;
+
+/* config test stuff
+config.test = {};
+config.test.setting = true;
+config.test.sub_settings = {};
+config.test.sub_settings.setting2 = 1337;
+config.test.sub_settings.setting3 = 2674;
+config.test.in_set = "testing";
+*/
+
+// override and defaults or process envrionment vars from config
+function mergeConfiguration(fromObj, toObj){
+    for (var property in fromObj) {
+        if (fromObj.hasOwnProperty(property)) {
+            // do stuff
+            if(typeof fromObj[property] == 'object' && toObj.hasOwnProperty(property)){
+                //sub property; recurse
+                mergeConfiguration(fromObj[property], toObj[property]);
+            }else if(toObj.hasOwnProperty(property)){
+                toObj[property] = fromObj[property];
+            }
+        }
+    }
+}
+mergeConfiguration(file_config, config);
+
+
+module.exports = config;
